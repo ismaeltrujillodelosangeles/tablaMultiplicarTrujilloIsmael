57e4be40f7d775b31fcf7a86b3cd94facdfebe16
ismaeltrujillodelosangeles <ismael.trujillo.delosangeles.alu@iesjulioverne.es>
2020-12-14 13:24:33 +0000

    Esta es la segunda versión, añado una tabla nueva

--- index.php
+++ index.php
@@ -3,4 +3,8 @@ for($i=0; $i<=10;$i++){
 	$resultado = $i*2;
 	echo("<br>2 x $i = $resultado");
 }
+for($i=0; $i<=10;$i++){
+        $resultado = $i*3;
+        echo("<br>3 x $i = $resultado");
+}
 ?>
